97ede579c85e63b537f4cde8cddd88132d366633
appolinaria07 <77410102+appolinaria07@users.noreply.github.com>
2021-02-17 14:45:11 -0800

    deleted meta tag

--- js/script.js
+++ js/script.js
@@ -3,4 +3,16 @@
 // function theMonth();
 // let currentMonth;
 // let summerMonth;
-// let myLibraryFinction;
+// let myLibraryFunction;
+
+// STEP 2 
+// example of numeric literal expression
+// -468 
+// "This is an example of string data"
+// example of Boolean literal expression
+// let x = 5000;
+// let y = 5000;
+// window.console.log(x == y);
+// example of null literal expression
+let x = null;
+window.console.log(x);
